229b3ef160e623e94a2ae7b4f46216bc242af9ed
hritvikkaushik <hritvikkaushik@gmail.com>
2025-09-26 15:07:27 +0530

    implemented --show-hidden flag, without which system files and hidden files will not be listed

--- ls-w32.cpp
+++ ls-w32.cpp
@@ -1,18 +1,26 @@
-// ls-w32.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <windows.h>
 #include <string>
 
-void ListFiles(const std::wstring& directory);
+void ListFiles(const std::wstring& directory, const bool showHidden);
 
 int wmain(int argc, wchar_t* argv[]) {
-    std::wstring path = (argc > 1) ? argv[1] : L".";
-    ListFiles(path);
+    std::wstring path = L".";
+    bool showHidden = false;
+
+    for (int i = 1; i < argc; i++) {
+        std::wstring arg = argv[i];
+        if (arg == L"--show-hidden") {
+            showHidden = true;
+        }
+        else {
+            path = arg;
+        }
+    }
+    ListFiles(path, showHidden);
 }
 
-void ListFiles(const std::wstring& directory) {
+void ListFiles(const std::wstring& directory, const bool showHidden) {
     std::wstring searchPath = directory + L"\\*";
     WIN32_FIND_DATAW findData;
     HANDLE hFind = FindFirstFileW(searchPath.c_str(), &findData);
@@ -29,11 +37,17 @@ void ListFiles(const std::wstring& directory) {
             continue;
         }
 
+        if (findData.dwFileAttributes & (FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM)) {
+            if (!showHidden) {
+                continue;
+            }
+        }
+
         std::wstring fullPath = directory + L"\\" + fileName;
 
         if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
             std::wcout << L"[DIR] " << fullPath << std::endl;
-            ListFiles(fullPath);
+            ListFiles(fullPath, showHidden);
         }
         else {
             std::wcout << L"        " << fullPath << std::endl;
