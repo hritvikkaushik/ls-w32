f596c8e3aff2cd462499b305d5714a798c88abad
hritvikkaushik <hritvikkaushik@gmail.com>
2025-09-26 15:13:08 +0530

    Implemented -r flag for recursive listing

--- ls-w32.cpp
+++ ls-w32.cpp
@@ -2,25 +2,29 @@
 #include <windows.h>
 #include <string>
 
-void ListFiles(const std::wstring& directory, const bool showHidden);
+void ListFiles(const std::wstring& directory, const bool& showHidden, const bool& recursive);
 
 int wmain(int argc, wchar_t* argv[]) {
     std::wstring path = L".";
     bool showHidden = false;
+    bool recursive = false;
 
     for (int i = 1; i < argc; i++) {
         std::wstring arg = argv[i];
         if (arg == L"--show-hidden") {
             showHidden = true;
         }
+        else if (arg == L"-r") {
+            recursive = true;
+        } 
         else {
             path = arg;
         }
     }
-    ListFiles(path, showHidden);
+    ListFiles(path, showHidden, recursive);
 }
 
-void ListFiles(const std::wstring& directory, const bool showHidden) {
+void ListFiles(const std::wstring& directory, const bool& showHidden, const bool& recursive) {
     std::wstring searchPath = directory + L"\\*";
     WIN32_FIND_DATAW findData;
     HANDLE hFind = FindFirstFileW(searchPath.c_str(), &findData);
@@ -47,7 +51,9 @@ void ListFiles(const std::wstring& directory, const bool showHidden) {
 
         if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
             std::wcout << L"[DIR] " << fullPath << std::endl;
-            ListFiles(fullPath, showHidden);
+            if (recursive) {
+                ListFiles(fullPath, showHidden, recursive);
+            }
         }
         else {
             std::wcout << L"        " << fullPath << std::endl;
